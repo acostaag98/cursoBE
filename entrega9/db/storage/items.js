0bd9e664e05994e625cc8e39854006d432a1619e
acostaag98 <acostaag98@gmail.com>
2021-07-27 02:12:03 -0300

    corregido

---
+++ entrega9/db/storage/items.js
@@ -0,0 +1,32 @@
+[
+	{
+		"id": 2,
+		"name": "Calculadora",
+		"price": 234.56,
+		"thumbnail": "www.google.com/2.png"
+	},
+	{
+		"id": 3,
+		"name": "Globo Terraqueo",
+		"price": 345.67,
+		"thumbnail": "www.google.com/3.png"
+	},
+	{
+		"id": 5,
+		"name": "Goma de borrar",
+		"price": 50,
+		"thumbnail": "www.google.com/5.png"
+	},
+	{
+		"id": 6,
+		"name": "Regla",
+		"price": "75",
+		"thumbnail": "www.google.com/regla.png"
+	},
+	{
+		"id": 7,
+		"name": "Agustín",
+		"price": "123",
+		"thumbnail": "www-asd.com"
+	}
+]
